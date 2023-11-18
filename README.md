8c8f4fe27a92e02c5a25dd076c7bdde9a2295e80
ErnestsLavrinovicsalias] <ernests.lavrinovics@gmail.com>
2023-11-18 14:02:22 +0100

    update runHpc script and env.yml

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # llamarizer
 Advanced NLP 2023 @ ITU Copenhagen course work
+
+## To run
+1. Start by creating the conda environment `conda env create -f environment.yml`
+and update it as necessary
+2. Run main.py
--- run_hpc.sh
+++ run_hpc.sh
@@ -1,15 +1,16 @@
-#!/usr/bin/bash
-# Script for executing the experiment on the HPC
-
-#SBATCH --job-name=adv_nlp_ebv # Job name
+#!/bin/bash
+#SBATCH --job-name=HelloHPC # Job name
 #SBATCH --output=simple.out # Name of output file
 #SBATCH --cpus-per-task=8 # Schedule one core
-#SBATCH --time=16:00:00 # Run time (hh:mm:ss)
+#SBATCH --time=24:00:00 # Run time (hh:mm:ss)
 #SBATCH --gres=gpu:rtx8000:1 # Request a GPU
 #SBATCH --partition=brown
-#SBATCH --output=jobLogs/job.%j.out # (%j expands to jobId)
-#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=job.%j.out # (%j expands to jobId)
+#SBATCH --mail-type=FAIL
 
 module purge
-module load Anaconda3
-source activate /home/$user/condaenv/advNlp
+conda init
+conda activate /home/$USER/.conda/envs/hackathon
+
+echo "Starting python"
+python3 main.py
