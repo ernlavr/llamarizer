d1be4b4120aa831eb64f5c30f7936eaac693cc5e
beba42 <beba@itu.dk>
2023-12-18 12:22:50 +0100

    Some changes in Summarization metrics

--- src/utils/SummarizationMetrics.py
+++ src/utils/SummarizationMetrics.py
@@ -16,14 +16,17 @@ nltk.download('stopwords')
 logging.disable(logging.WARNING)
 
 class FactCC:
-    def __init__(self, model_path='manueldeprada/FactCC'):
+    def __init__(self, device = "cuda", model_path='manueldeprada/FactCC'):
         """
         Initializes the FactCC class.
 
         Args:
             model_path (str): The path or name of the pre-trained model to be used. Defaults to 'manueldeprada/FactCC'.
         """
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if device == "cuda":
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        else:
+            self.device = torch.device("cpu")
         self.tokenizer = BertTokenizer.from_pretrained(model_path)
         self.model = BertForSequenceClassification.from_pretrained(model_path).to(self.device)
         self.model.eval()
@@ -42,11 +45,13 @@ class FactCC:
             predictions = []
             for pred, ref in to_eval:
                 input_dict = self.tokenizer(pred, ref, max_length=512, padding='max_length', truncation='longest_first', return_tensors='pt')
+                input_dict = {key: val.to(self.device) for key, val in input_dict.items()}
+
                 logits = self.model(**input_dict).logits
                 softmax_probs = torch.nn.functional.softmax(logits, dim=1)
                 predictions.append(softmax_probs[0][0].item())
                      
-            FactCC_score = np.mean(predictions)
+            FactCC_score = predictions
             return FactCC_score
 
 class ANLI:
@@ -79,20 +84,22 @@ class ANLI:
                             labels=None)
             predicted_probability = torch.softmax(outputs[0], dim=1)[0].tolist()
             preds.append(predicted_probability[0])
-        return np.mean(preds)
+        return preds
 
 
 class SummaC:
 
-    def __init__(self):
-
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    def __init__(self,device = "cuda"):
+        if device == "cuda":
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        else: 
+            self.device = torch.device("cpu")
         self.model = SummaCConv(models=["vitc"], bins='percentile', granularity="sentence", nli_labels="e", device=self.device, start_file="default", agg="mean") 
 
     def compute(self, sources, summaries):
         scores = self.model.score(sources, summaries)
         print(scores['scores'])
-        return np.mean(scores['scores'])
+        return scores['scores']
     
 class CTC:
     def __init__(self):
@@ -121,6 +128,8 @@ class SummarizationMetrics:
             stop_words = set(stopwords.words('english'))
             tokens = [token.lower() for token in word_tokenize(text) if token.isalnum() and token.lower() not in stop_words]
             return tokens
+    def remove_padding(self,text):
+        return text.replace("<s>", "").replace("</s>", "")
 
     def calculate_novel_ngram_ratio(self, source_text, summary, n=2):
             """
@@ -141,6 +150,8 @@ class SummarizationMetrics:
             summary_ngrams = list(ngrams(summary_tokens, n))
             source_freq_dist = FreqDist(source_ngrams)
             novel_ngrams_count = sum(1 for ngram in summary_ngrams if ngram not in source_freq_dist)
+            if len(summary_ngrams) == 0:
+                return 0
             novel_ngram_ratio = novel_ngrams_count / len(summary_ngrams)
             return novel_ngram_ratio
 
@@ -155,11 +166,12 @@ class SummarizationMetrics:
             Returns:
                 float: The redundancy score, ranging from 0 to 1. A score of 0 indicates no redundancy, while a score of 1 indicates maximum redundancy.
             """
-            summary_tokens = self.preprocess_text(summary)
+            summary_tokens = tokens = [token.lower() for token in word_tokenize(summary) if token.isalnum() and token.lower()]
             summary_ngrams = list(ngrams(summary_tokens, n))
             freq_dist = FreqDist(summary_ngrams)
             repeated_ngrams_count = sum(fi - 1 for fi in freq_dist.values())
             total_ngrams_count = len(summary_ngrams)
+
             red_score = 1 - (repeated_ngrams_count / total_ngrams_count)
             return red_score
 
@@ -203,8 +215,11 @@ class SummarizationMetrics:
         novel_2gram_ratios = []
         novel_3gram_ratios = []
         compression_scores = []
-
+        
         for source_text, summary in zip(source_texts, summarys):
+        
+            source_text = self.remove_padding(source_text)
+            summary = self.remove_padding(summary)
             red_score = self.calculate_red_score(summary)
             red_scores.append(red_score)
             novel_1gram_ratio = self.calculate_novel_ngram_ratio(source_text, summary, n=1)
@@ -217,11 +232,11 @@ class SummarizationMetrics:
             compression_scores.append(compression_score)
 
         return {
-            "red_score": sum(red_scores) / len(red_scores),
-            "novel_1gram_ratio": sum(novel_1gram_ratios) / len(novel_1gram_ratios),
-            "novel_2gram_ratio": sum(novel_2gram_ratios) / len(novel_2gram_ratios),
-            "novel_3gram_ratio": sum(novel_3gram_ratios) / len(novel_3gram_ratios),
-            "compression_score": sum(compression_scores) / len(compression_scores),
+            "red_score": red_scores,
+            "novel_1gram_ratio": novel_1gram_ratios,
+            "novel_2gram_ratio": novel_2gram_ratios,
+            "novel_3gram_ratio": novel_3gram_ratios,
+            "compression_score": compression_scores,
         }
 
 
